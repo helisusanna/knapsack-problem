2c38bade700cd464da60e874fafcffee4bf2d4bd
Heli Hyttinen <bhehy002@edu.xamk.fi>
2021-12-19 20:28:42 +0200

    changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const {spawn} = require('child_process');
 const bodyParser = require("body-parser");
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 app.set("views", "./views");
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
